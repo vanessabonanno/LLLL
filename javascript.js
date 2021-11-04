f2f4f1fb03965baad8227fedb7551f90eada3d7c
SarahTew <72857758+SarahTew@users.noreply.github.com>
2021-11-04 11:46:35 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//this function toggles the display of the columns for each article
+//still has an issue with not working on first click
 function myFunction(id) {
   var x = document.getElementById(id);
   if (x.style.display === "block") {
@@ -7,8 +9,21 @@ function myFunction(id) {
   }
 }
 
-//This is experimental//
-function toggle_rel(id) {
-  var x = document.getElementById(id);
-  x.style.color= "pink";
+//this function allows stuff to be added and removed below the checkboxes in the metadataviewer.
+//we need another function that can automatically write the html code according to which subheadings we want
+function test1(el){
+	// if div with id="subheadings" is empty
+	if (!$.trim($('div#subheadings').html())) {
+    //make an object with whatever we want inside
+		var obj = {
+		content : "<ul><li>coffee</li><li>tea</li></ul>'"
+		};
+	//this part is what makes it handle the html tags correctly
+		$('#subheadings').append(obj.content);
+	}
+	//else remove all contents from the div so it's empty
+	//unlike myFunction, the div is truly empty, not just hidden
+	else {
+		document.getElementById("subheadings").innerHTML = "";
+		}
 }
