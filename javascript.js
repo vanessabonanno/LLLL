b53439933946b1b2ead57186b435f94d1cca5940
SarahTew <72857758+SarahTew@users.noreply.github.com>
2021-11-04 11:50:58 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -12,17 +12,13 @@ function myFunction(id) {
 //this function allows stuff to be added and removed below the checkboxes in the metadataviewer.
 //we need another function that can automatically write the html code according to which subheadings we want
 function test1(el){
-	// if div with id="subheadings" is empty
 	if (!$.trim($('div#subheadings').html())) {
-    //make an object with whatever we want inside
+    // if div with id="subheadings" is empty, your code goes here
 		var obj = {
 		content : "<ul><li>coffee</li><li>tea</li></ul>'"
 		};
-	//this part is what makes it handle the html tags correctly
 		$('#subheadings').append(obj.content);
 	}
-	//else remove all contents from the div so it's empty
-	//unlike myFunction, the div is truly empty, not just hidden
 	else {
 		document.getElementById("subheadings").innerHTML = "";
 		}
