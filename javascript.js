3fc14181ff96a23b7b579786c815e075f1d6308e
SarahTew <72857758+SarahTew@users.noreply.github.com>
2021-11-19 12:47:51 +0100

    Add files via upload
    renamed js functions
    added ids and function inside html to load titles

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function myFunction(id, id2) {
+function edData(id, id2) {
   var x = document.getElementById(id);
   var y = document.getElementById(id2);
   if (x.style.display === "block") {
@@ -11,7 +11,13 @@ function myFunction(id, id2) {
   
 }
 
-function myFunction3(id, id2, subclass) {
+function getTitles() {
+	
+	document.getElementById("label1").innerText = document.getElementById("title1").innerText;
+	document.getElementById("label2").innerText = document.getElementById("title2").innerText;
+	document.getElementById("label3").innerText = document.getElementById("title3").innerText;
+}
+function getSubheadings(id, id2, subclass) {
 	var x = document.getElementById(id);
 	var y = document.getElementById(id2);
 	const sub = document.getElementsByClassName(subclass);
@@ -33,26 +39,9 @@ function myFunction3(id, id2, subclass) {
 }
 
 
-function myfunction6(value) {
-	var sub = document.querySelectorAll('[about="' + value + '"]'); //works
-	var x = document.getElementById(value);
-	
-	if (x.hasAttribute("donkey")){ //this is the line that doesn't work how to check if the box is checked or not
-			for(let i = 0; i < sub.length; i++) {
-				sub[i].classList.add('highlight');
-			}
-		x.removeAttribute("donkey");
-	}
-	else{
-		for(let i = 0; i < sub.length; i++) {
-		sub[i].classList.remove('highlight');
-		}
-		x.setAttribute("donkey", "kong");
-	}
 
-}
 
-function myFunction8(id2, class_id) {
+function getMetadata(id2, class_id) {
 	var y = document.getElementById(id2); //this is where the metadata info will be displayed
 	
 	var sub1 = document.getElementById('col1').getElementsByClassName(class_id);
@@ -68,11 +57,11 @@ function myFunction8(id2, class_id) {
 		}
 		for(let i=0; i< sub2.length; i++) {
 			//sub2[i].removeAttribute('style');
-			sub1[i].classList.remove('highlight');
+			sub2[i].classList.remove('highlight');
 		}
 		for(let i=0; i< sub3.length; i++) {
 			//sub3[i].removeAttribute('style');//'style' must be changed to a css id or class. Need to find a way to reload the css, however, or it won't work
-			sub1[i].classList.remove('highlight');
+			sub3[i].classList.remove('highlight');
 		}
 	}
 	else {
@@ -91,7 +80,7 @@ function myFunction8(id2, class_id) {
 			if(item !== null && item !== '' && !((used_labels.includes(item)))) {
 			
 		
-		subtext += "<label><input type='checkbox' checked = 'true' onclick=\"myfunction6('" + sub[i].getAttribute('about') + "')\" id='" + sub[i].getAttribute('about') + "'><a target='_blank' href='" + sub[i].getAttribute('url') + "'>" + sub[i].getAttribute('data-label') + "</a></label><br>"
+		subtext += "<label><input type='checkbox' checked = 'true' onclick=\"toggleHighlight('" + sub[i].getAttribute('about') + "')\" id='" + sub[i].getAttribute('about') + "'><a target='_blank' href='" + sub[i].getAttribute('url') + "'>" + sub[i].getAttribute('data-label') + "</a></label><br>"
 		used_labels.push(item);
 			}
 		}
@@ -107,7 +96,7 @@ function myFunction8(id2, class_id) {
 			if(item !== null && item !== '' && !((used_labels.includes(item)))) {
 			
 		
-		subtext += "<label><input type='checkbox' checked = 'true' onclick=\"myfunction6('" + sub[i].getAttribute('about') + "')\" id='" + sub[i].getAttribute('about') + "'><a target='_blank' href='" + sub[i].getAttribute('url') + "'>" + sub[i].getAttribute('data-label') + "</a></label><br>"
+		subtext += "<label><input type='checkbox' checked = 'true' onclick=\"toggleHighlight('" + sub[i].getAttribute('about') + "')\" id='" + sub[i].getAttribute('about') + "'><a target='_blank' href='" + sub[i].getAttribute('url') + "'>" + sub[i].getAttribute('data-label') + "</a></label><br>"
 
 			}
 		}
@@ -122,7 +111,7 @@ function myFunction8(id2, class_id) {
 			if(item !== null && item !== '' && !((used_labels.includes(item)))) {
 			
 	
-			subtext += "<label><input type='checkbox' checked = 'true' onclick=\"myfunction6('" + sub[i].getAttribute('about') + "')\" id='" + sub[i].getAttribute('about') + "'><a target='_blank' href='" + sub[i].getAttribute('url') + "'>" + sub[i].getAttribute('data-label') + "</a></label><br>"
+			subtext += "<label><input type='checkbox' checked = 'true' onclick=\"toggleHighlight('" + sub[i].getAttribute('about') + "')\" id='" + sub[i].getAttribute('about') + "'><a target='_blank' href='" + sub[i].getAttribute('url') + "'>" + sub[i].getAttribute('data-label') + "</a></label><br>"
 			}
 			}
 		}
@@ -130,3 +119,21 @@ function myFunction8(id2, class_id) {
   }
 }
 
+function toggleHighlight(value) {
+	var sub = document.querySelectorAll('[about="' + value + '"]'); //works
+	var x = document.getElementById(value);
+	
+	if (x.hasAttribute("donkey")){ //this is the line that doesn't work how to check if the box is checked or not
+			for(let i = 0; i < sub.length; i++) {
+				sub[i].classList.add('highlight');
+			}
+		x.removeAttribute("donkey");
+	}
+	else{
+		for(let i = 0; i < sub.length; i++) {
+		sub[i].classList.remove('highlight');
+		}
+		x.setAttribute("donkey", "kong");
+	}
+
+}
